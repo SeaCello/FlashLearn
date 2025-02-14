d9c19a146c7408e15750fade5b1c33ed17605d35
Marcello Eduardo Alves de Menezes <meam@WINDOWS.CIN.UFPE.BR>
2025-02-14 11:12:39 -0300

    tailwind setup and frontpage

--- app/webapp/settings.py
+++ app/webapp/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tailwind',
+    'theme',
+    'django_browser_reload',
 ]
 
 AUTH_USER_MODEL = 'auth.User'  
@@ -56,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = 'webapp.urls'
@@ -136,3 +140,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 from dotenv import load_dotenv #import the load_dotenv function
 load_dotenv()
+
+# tailwind settings
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+NPM_BIN_PATH = 'C:/Program Files/nodejs/npm.cmd'
