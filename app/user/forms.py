906d60c20c81158ce9493e45de7e8209836da7f8
Eduardo <161507924+Eduardocin@users.noreply.github.com>
2025-03-28 13:09:10 -0300

    corrijido coerência entre login e registro

--- app/user/forms.py
+++ app/user/forms.py
@@ -1,24 +1,30 @@
 from django import forms
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from crispy_forms.layout import Layout, Div, Submit
-from crispy_forms.helper import FormHelper
+from django.contrib.auth.models import User
 
 class CustomUserCreationForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    username = forms.CharField(
+        label='Nome de usuário',
+        widget=forms.TextInput(attrs={'placeholder': 'Digite seu nome de usuário'})
+    )
+    email = forms.EmailField(
+        label='E-mail',
+        widget=forms.EmailInput(attrs={'placeholder': 'Digite seu e-mail'})
+    )
+    password1 = forms.CharField(
+        label='Senha',
+        widget=forms.PasswordInput(attrs={'placeholder': '••••••••'})
+    )
+    password2 = forms.CharField(
+        label='Confirme sua senha',
+        widget=forms.PasswordInput(attrs={'placeholder': '••••••••'})
+    )
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
-
+        fields = ['username', 'email', 'password1', 'password2']
+    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.helper = FormHelper()
-        self.helper.form_class = 'border p-8'
-        self.helper.layout = Layout(
-            'username',
-            'email',
-            'password1',
-            'password2',
-            Submit('submit', 'Registrar', css_class="w-full text-white bg-orange-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-primary-800"),
-        )
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
